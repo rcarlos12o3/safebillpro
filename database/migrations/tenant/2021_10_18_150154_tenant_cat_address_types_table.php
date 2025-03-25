9b407169ef37177afbd86657b3dd97e20355648a
DevCortes <dante.e.cortes@gmail.com>
2025-03-25 18:09:34 -0500

    Add increments in payment conditions table.

--- database/migrations/tenant/2021_10_18_150154_tenant_cat_address_types_table.php
+++ database/migrations/tenant/2021_10_18_150154_tenant_cat_address_types_table.php
@@ -15,6 +15,7 @@ class TenantCatAddressTypesTable extends Migration
     { 
 
         Schema::create('cat_address_types', function (Blueprint $table) {
+            $table->increments('inidice');
             $table->string('id')->primary();
             $table->string('description');
         });
